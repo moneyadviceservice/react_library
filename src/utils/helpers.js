1d6c3ff128dffbe50e2942638c84dd0b8fa0798b
andreferreiradlw <ferreirandre25@gmail.com>
2020-01-23 12:26:07 +0000

    Background Helper Function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -73,7 +73,6 @@ export const borderStyle = data => {
       border: ${theme.sizes.borderSize[data] || data};
     `
   }
-
   const styles = []
   const color = data.color || 'black'
   const borderSize = data.size || 'xsmall' // 1px
@@ -106,6 +105,98 @@ export const borderStyle = data => {
     `)
   }
   return styles
+}
+
+export const backgroundStyle = (backgroundArg, textColorArg) => {
+  const textColor = textColorArg || theme.colors
+  const opacity =
+    theme.colors.opacity[backgroundArg.opacity] || backgroundArg.opacity || 1
+  /** if background is object */
+  if (typeof backgroundArg === 'object') {
+    const styles = []
+    //** if background is an image */
+    if (backgroundArg.image) {
+      let color
+      if (backgroundArg.dark === false) {
+        color = textColor.dark
+      } else if (backgroundArg.dark) {
+        color = textColor.light
+      } else if (!textColorArg) {
+        color = 'inherit'
+      }
+      styles.push(css`
+        background-image: ${backgroundArg.image};
+        background-repeat: ${backgroundArg.repeat || 'no-repeat'};
+        background-position: ${backgroundArg.position || 'center center'};
+        background-size: ${backgroundArg.size || 'cover'};
+        color: ${color};
+      `)
+      console.log('image!', styles)
+    }
+    //** if background is a color */
+    if (backgroundArg.color) {
+      const color = backgroundArg.color
+      /** if color in hex, convert to rgba with opacity */
+      const backgroundColor =
+        (typeof color === 'string' && hexToRGB(color, opacity)) || color
+      console.log('colors!', color, backgroundColor)
+      styles.push(css`
+        background-color: ${backgroundColor};
+      `)
+    }
+    /** if background dark change font color */
+    if (backgroundArg.dark === false) {
+      styles.push(css`
+        color: ${textColor.light || textColor};
+      `)
+    } else if (backgroundArg.dark) {
+      styles.push(css`
+        color: ${textColor.dark || textColor};
+      `)
+    }
+    return styles
+  }
+  /** if background is a string */
+  if (typeof backgroundArg === 'string') {
+    /** url */
+    if (backgroundArg.lastIndexOf('url', 0) === 0) {
+      return css`
+        background: ${background} no-repeat center center;
+        background-size: cover;
+      `
+    } else {
+      /** color value */
+      return css`
+        background: ${backgroundArg};
+      `
+    }
+  }
+  return undefined
+}
+
+function hexToRGB(hex, alpha = 1) {
+  // allow for alpha: #RGB, #RGBA, #RRGGBB, or #RRGGBBAA
+  const hexExp = /^#[A-Za-z0-9]{3,4}$|^#[A-Za-z0-9]{6,8}$/
+  const rgbExp = /rgba?\(\s?([0-9]*)\s?,\s?([0-9]*)\s?,\s?([0-9]*)\s?\)/
+  const rgbaExp = /rgba?\(\s?([0-9]*)\s?,\s?([0-9]*)\s?,\s?([0-9]*)\s?,\s?([.0-9]*)\s?\)/
+  // e.g. hsl(240, 60%, 50%)
+  const hslExp = /hsla?\(\s?([0-9]*)\s?,\s?([0-9]*)%?\s?,\s?([0-9]*)%?\s?.*?\)/
+  // check if it's a hex value or string
+  const canExtractRGBArray = color =>
+    hexExp.test(color) ||
+    rgbExp.test(color) ||
+    rgbaExp.test(color) ||
+    hslExp.test(color)
+
+  if (canExtractRGBArray(hex)) {
+    var r = parseInt(hex.slice(1, 3), 16),
+      g = parseInt(hex.slice(3, 5), 16),
+      b = parseInt(hex.slice(5, 7), 16)
+
+    return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')'
+  } else {
+    return false
+  }
 }
 
 export const genericStyles = css`
