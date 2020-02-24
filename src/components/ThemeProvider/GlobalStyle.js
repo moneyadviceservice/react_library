4bd72ac7f2ff5beb924a6209e017fa502f6f4c30
andreferreiradlw <ferreirandre25@gmail.com>
2020-02-24 11:07:11 +0000

    Adds Anchor and Header Styles to Theme

--- src/components/ThemeProvider/GlobalStyle.js
+++ src/components/ThemeProvider/GlobalStyle.js
@@ -48,9 +48,36 @@ const GlobalStyle = createGlobalStyle`
     font-size: inherit;
     line-height: inherit;
   }
-  a,
-  a:visited {
-    color: inherit;
+  // anchor
+  a {
+    color: ${props => props.theme.colors.linkDefault};
+    text-decoration: none;
+
+    &:visited {
+      color: inherit;
+    }
+
+    &:hover,
+    &:focus {
+      color: inherit;
+      text-decoration: underline;
+    }
+
+    &:hover {
+      outline: none;
+    }
+
+    &:focus {
+      outline: solid 0.1875rem ${props => props.theme.colors.focusBg};
+      color: ${props => props.theme.colors.focusBg};
+    }
+
+    // Removes active link colour that is applied in Mobile Safari and adds kerning
+    &[href^=tel] {
+      color: inherit;
+      letter-spacing: -1px;
+      text-decoration: none;
+    }
   }
 
   // Layout & box sizing
