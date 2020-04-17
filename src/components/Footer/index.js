731891dc87a6f790c60319eaadd685ceb68e3712
andreferreiradlw <ferreirandre25@gmail.com>
2020-04-17 12:28:17 +0100

    Adds new components to Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,28 +2,15 @@ import React from 'react'
 import { genericPropTypes, genericPropsDefaults } from '../../utils/prop-types'
 import { FooterContainer } from './StyledFooter'
 import ContactPanels from './ContactPanels'
+import FooterPrimary from './FooterPrimary'
+import FooterSecondary from './FooterSecondary'
 
 const Footer = ({ as, direction, ...rest }) => {
   return (
     <FooterContainer fluid as="footer" {...rest}>
       <ContactPanels />
-      {/* <FooterPrimary>
-        <SocialMediaLinks sizes={{ xs: 12, md: 6 }} debug>
-          twiter
-        </SocialMediaLinks>
-
-        <FooterNav sizes={{ xs: 12, sm: 8, md: 4 }} debug>
-          links
-        </FooterNav>
-        <FooterLinks sizes={{ xs: 12, sm: 4, md: 2 }} debug>
-          images
-        </FooterLinks>
-
-        <Copyright>copyright</Copyright>
-      </FooterPrimary>
-      <FooterSecondary debug>
-        <Col>last row</Col>
-      </FooterSecondary> */}
+      <FooterPrimary />
+      <FooterSecondary />
     </FooterContainer>
   )
 }
