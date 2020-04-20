07b7de5111eeb3cedc603cd63bf4b7ebe221ad1d
andreferreiradlw <ferreirandre25@gmail.com>
2020-04-20 16:12:54 +0100

    Footer Secondary Content + Styles

--- src/components/Footer/FooterSecondary/StyledFooterSecondary.js
+++ src/components/Footer/FooterSecondary/StyledFooterSecondary.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 import { resolveMedia } from '../../../utils/helpers'
 import { Row, Col } from '../../Grid'
+import { Anchor } from '../../Anchor'
+import accessibility_svg from '../../../assets/Images/icon_accessibility.svg'
 
+// container row
 const FooterSecondaryContainer = styled(Row)`
   ${props =>
     !props.background && `background-color: ${props.theme.colors.mapsBlue};`}
@@ -9,44 +12,80 @@ const FooterSecondaryContainer = styled(Row)`
   color: white;
 `
 
-const Translation = styled(Col)`
-  flex-grow: 0;
-
-  ${resolveMedia.md`
-    display: none;
-  `}
-`
-
+// accessibility section
 const Acessibility = styled(Col)`
   flex-grow: 0;
   font-size: 0.875rem;
   line-height: 1.5rem;
 `
 
-const MapsLink = styled(Col)`
-  flex-grow: 0;
+const AccessibilityIcon = styled(accessibility_svg)`
+  width: 16px;
+  height: 16px;
+  margin-right: 4px;
 `
 
+// maps logo section
+const MapsText = styled.span`
+  display: inline-block;
+
+  & span {
+    display: block;
+    font-size: 0.875rem;
+    line-height: 1rem;
+  }
+`
+
+const MapsLink = styled(Col)`
+  flex-grow: 0;
+  margin: 12px 0 6px;
+
+  & svg {
+    width: 120px;
+    margin-left: 10px;
+  }
+`
+
+// link list section
 const ListContainer = styled(Col)`
   flex-grow: 0;
-  ${resolveMedia.md`
-    ${props => !props.margin && `margin-left: auto;`}
-  `}
 `
 
 const FooterSecondaryList = styled.ul`
   display: flex;
   flex-wrap: inherit;
+  margin: 6px 0;
+
+  &:not(:last-child) {
+    margin-right: 6px;
+  }
 `
 
-const FooterSecondaryListItem = styled.li``
+const FooterSecondaryListItem = styled.li`
+  padding-right: 12px;
+  margin-top: 6px;
+`
+
+const FooterAnchor = styled(Anchor)`
+  color: white;
+`
+
+// mobile translation section
+const Translation = styled(Acessibility)`
+  ${resolveMedia.md`
+    display: none;
+  `}
+`
 
 export {
   FooterSecondaryContainer,
   Translation,
   Acessibility,
+  AccessibilityIcon,
+  MapsText,
   MapsLink,
   ListContainer,
   FooterSecondaryList,
   FooterSecondaryListItem,
+  FooterAnchor,
 }
