9c142d5d91307054411ffb9dfa1264fd825b77e6
andreferreiradlw <ferreirandre25@gmail.com>
2020-04-27 14:12:52 +0100

    Initial Styleguidist Setup

---
+++ src/components/Form/Form.md
@@ -0,0 +1,9 @@
+Renders a Form element.
+
+```jsx
+import { Form } from '@moneypensionservice/directories';
+
+<Form>
+  I'm a Form
+</Form>
+```
---
+++ src/components/Form/StyledForm.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import { Col } from '../Grid'
+
+const StyledForm = styled(Col)``
+
+export default StyledForm
