6efe051a1c70f3b4a35e56d67de925c7cd2ff67d
andreferreiradlw <ferreirandre25@gmail.com>
2020-04-29 15:21:59 +0100

    Footer Locale Context

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,24 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { genericPropTypes, genericPropsDefaults } from '../../utils/prop-types'
 import { FooterContainer } from './StyledFooter'
+// footer panels
 import ContactPanels from './ContactPanels'
 import FooterPrimary from './FooterPrimary'
 import FooterSecondary from './FooterSecondary'
+// translations
+import LocaleEn from './locale_en.json'
+import LocaleCy from './locale_cy.json'
+import LocaleContext from './LocaleContext'
 
-const Footer = ({ as, direction, ...rest }) => {
+const Footer = ({ currentLgn, ...rest }) => {
+  const i18n = currentLgn === 'en' ? LocaleEn : LocaleCy
+
   return (
-    <FooterContainer fluid as="footer" {...rest}>
-      <ContactPanels />
-      <FooterPrimary />
-      <FooterSecondary />
-    </FooterContainer>
+    <LocaleContext.Provider value={i18n}>
+      <FooterContainer forwardedAs="footer" fluid {...rest}>
+        <ContactPanels />
+        <FooterPrimary />
+        <FooterSecondary />
+      </FooterContainer>
+    </LocaleContext.Provider>
   )
 }
 
 Footer.prototypes = {
+  /** Current Language Value */
+  currentLgn: PropTypes.oneOf(['en', 'cy']),
   ...genericPropTypes,
 }
 Footer.defaultProps = {
+  currentLgn: 'en',
   ...genericPropsDefaults(),
 }
 
