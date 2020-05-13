643086f1bb2c056604de5beed3b8fbdc086c8aec
andreferreiradlw <ferreirandre25@gmail.com>
2020-05-13 14:53:27 +0100

    Adds disabled color to Checkbox

--- src/components/Checkbox/StyledCheckbox.js
+++ src/components/Checkbox/StyledCheckbox.js
@@ -4,12 +4,14 @@ import { genericStyles } from '../../utils/helpers'
 const Label = styled.label`
   ${genericStyles}
   ${props => !props.margin && `margin-bottom: 0.75rem;`}
+  ${props => props.disabled && `color: ${props.theme.colors.disabledText};`}
   font-size: 1rem;
   line-height: 1.375rem;
   display: block;
 `
 
 const Field = styled.input`
+  margin-right: 5px;
   padding: 0;
 `
 
