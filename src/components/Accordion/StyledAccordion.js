b520fd1d52d2a6b8cc27a9cd1ce015481d06bdcd
andreferreiradlw <ferreirandre25@gmail.com>
2020-06-01 15:51:35 +0100

    Implements opened accordion state

--- src/components/Accordion/StyledAccordion.js
+++ src/components/Accordion/StyledAccordion.js
@@ -1,12 +1,30 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Col } from '../Grid'
 import { Paragraph } from '../Paragraph'
 
 // Accordion container
-const StyledAccordion = styled(Col)``
+const StyledAccordion = styled(Col)`
+  ${({ width }) => !width && `width: 100%;`}
+`
 
 const ContentContainer = styled(Col)`
   ${({ padding }) => !padding && `padding: 0;`}
+  height: 0px;
+  overflow: hidden;
+  transition: ease-out 0.3s;
+  opacity: 0;
+
+  ${({ show }) =>
+    show &&
+    css`
+      opacity: 1;
+    `}
+
+  &::after {
+    content: '';
+    display: block;
+    clear: both;
+  }
 `
 
 const Content = styled(Paragraph)`
--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -1,27 +1,65 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { genericPropTypes, genericPropsDefaults } from '../../utils/prop-types'
 import { Anchor } from '../Anchor'
 import { Content, ContentContainer, StyledAccordion } from './StyledAccordion'
 
-const Accordion = ({ a11yTitle, children, text, title, ...rest }) => {
+const Accordion = ({
+  a11yTitle,
+  children,
+  text,
+  title,
+  onChange,
+  openByDefault,
+  ...rest
+}) => {
   const [open, setOpen] = useState(false)
+  const contentRef = useRef(null)
+  let currentContent = null
+
+  useEffect(() => {
+    currentContent.style.height = open
+      ? `${currentContent.scrollHeight}px`
+      : (currentContent.style.height = '0')
+  }, [contentRef, open])
 
   return (
     <StyledAccordion aria-label={a11yTitle} {...rest}>
-      <Anchor onClick={() => setOpen(!open)}>{title}</Anchor>
-      {text && (
-        <ContentContainer>
-          <Content>{text}</Content>
-        </ContentContainer>
-      )}
-      {children}
+      <Anchor
+        onClick={() => {
+          setOpen(!open)
+          if (onChange) onChange()
+        }}
+      >
+        {title}
+      </Anchor>
+      <ContentContainer
+        show={open}
+        ref={c => {
+          currentContent = c
+        }}
+      >
+        {text && <Content>{text}</Content>}
+        {children}
+      </ContentContainer>
     </StyledAccordion>
   )
 }
 
 // Documentation
 Accordion.propTypes = {
+  // Color for the chevron
+  color: PropTypes.string,
+  // The children elements to render within the acordion
+  children: PropTypes.node,
+  // The text to render inside the accordion
+  text: PropTypes.string,
+  // Title of the accordion
+  title: PropTypes.string,
+  // Callback when the accordion state changes
+  onChange: PropTypes.func,
+  // Set accordion to be opened by default
+  openByDefault: PropTypes.bool,
   ...genericPropTypes,
 }
 
