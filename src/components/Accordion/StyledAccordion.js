adea7e3f251eb6f49e45f04b05b1f9157f3416aa
andreferreiradlw <ferreirandre25@gmail.com>
2020-06-02 11:32:08 +0100

    Finalises Accordion Component
    Adds custom color support, chevron and openend by default functionality

--- src/components/Accordion/StyledAccordion.js
+++ src/components/Accordion/StyledAccordion.js
@@ -1,21 +1,51 @@
 import styled, { css } from 'styled-components'
 import { Col } from '../Grid'
 import { Paragraph } from '../Paragraph'
+import Chevron from '../../assets/Images/chevron.svg'
 
 // Accordion container
 const StyledAccordion = styled(Col)`
-  ${({ width }) => !width && `width: 100%;`}
+  ${({ width }) =>
+    !width &&
+    css`
+      width: 100%;
+    `}
 `
 
+// Accordion Button
+const AccordionBtn = styled(Col)`
+  cursor: pointer;
+  padding: 10px 0;
+  border: none;
+  outline: none;
+  background-color: white;
+  transition: background-color 0.6s ease;
+`
+
+// Icon
+const Icon = styled(Chevron)`
+  width: 12px;
+  margin-right: 10px;
+  transition: transform ease-out 0.3s;
+  ${({ fillColor, theme }) => css`
+    fill: ${fillColor ? fillColor : theme.colors.accordion.default};
+  `}
+
+  ${({ isOpen }) =>
+    isOpen &&
+    css`
+      transform: rotate(90deg);
+    `}
+`
+
+// Content
 const ContentContainer = styled(Col)`
-  ${({ padding }) => !padding && `padding: 0;`}
   height: 0px;
   overflow: hidden;
   transition: ease-out 0.3s;
   opacity: 0;
-
-  ${({ show }) =>
-    show &&
+  ${props =>
+    props.show &&
     css`
       opacity: 1;
     `}
@@ -28,8 +58,25 @@ const ContentContainer = styled(Col)`
 `
 
 const Content = styled(Paragraph)`
-  ${({ border }) => !border && `border-left: 4px solid #027db4;`}
-  ${({ padding }) => !padding && `padding: 2px 8px;`}
+  ${({ border, borderColor, theme }) =>
+    !border &&
+    css`
+      border-left-width: 5px;
+      border-left-style: solid;
+      border-left-color: ${borderColor
+        ? borderColor
+        : theme.colors.accordion.default};
+    `}
+  ${({ padding }) =>
+    !padding &&
+    css`
+      padding-left: 10px;
+    `}
+  ${({ margin }) =>
+    !margin &&
+    css`
+      margin: 0;
+    `}
 `
 
-export { Content, ContentContainer, StyledAccordion }
+export { AccordionBtn, Content, ContentContainer, Icon, StyledAccordion }
--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -2,18 +2,25 @@ import React, { useState, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { genericPropTypes, genericPropsDefaults } from '../../utils/prop-types'
 import { Anchor } from '../Anchor'
-import { Content, ContentContainer, StyledAccordion } from './StyledAccordion'
+import {
+  AccordionBtn,
+  Content,
+  ContentContainer,
+  Icon,
+  StyledAccordion,
+} from './StyledAccordion'
 
 const Accordion = ({
   a11yTitle,
   children,
+  color,
   text,
   title,
   onChange,
   openByDefault,
   ...rest
 }) => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(openByDefault)
   const contentRef = useRef(null)
   let currentContent = null
 
@@ -24,22 +31,34 @@ const Accordion = ({
   }, [contentRef, open])
 
   return (
-    <StyledAccordion aria-label={a11yTitle} {...rest}>
-      <Anchor
+    <StyledAccordion
+      forwardedAs="section"
+      aria-label={a11yTitle || title}
+      {...rest}
+    >
+      <AccordionBtn
+        forwardedAs="button"
+        aria-expanded={open}
+        direction="row"
+        align="center"
+        justify="flex-start"
         onClick={() => {
           setOpen(!open)
           if (onChange) onChange()
         }}
       >
-        {title}
-      </Anchor>
+        <Icon fillColor={color} isOpen={open} />
+        <Anchor color={color} margin="0" width="auto">
+          {title}
+        </Anchor>
+      </AccordionBtn>
       <ContentContainer
         show={open}
         ref={c => {
           currentContent = c
         }}
       >
-        {text && <Content>{text}</Content>}
+        {text && <Content borderColor={color}>{text}</Content>}
         {children}
       </ContentContainer>
     </StyledAccordion>
@@ -48,22 +67,23 @@ const Accordion = ({
 
 // Documentation
 Accordion.propTypes = {
-  // Color for the chevron
+  /** Color for the chevron. */
   color: PropTypes.string,
-  // The children elements to render within the acordion
+  /** The children elements to render within the acordion. */
   children: PropTypes.node,
-  // The text to render inside the accordion
+  /** The text to render inside the accordion. */
   text: PropTypes.string,
-  // Title of the accordion
+  /** Title of the accordion. */
   title: PropTypes.string,
-  // Callback when the accordion state changes
+  /** Callback when the accordion state changes. */
   onChange: PropTypes.func,
-  // Set accordion to be opened by default
+  /** Set accordion to be opened by default. */
   openByDefault: PropTypes.bool,
   ...genericPropTypes,
 }
 
 Accordion.defaultProps = {
+  openByDefault: false,
   ...genericPropsDefaults(),
 }
 
