aa50c751fc1ec82683ee0ae83cf2a13a247e5b6d
andreferreiradlw <ferreirandre25@gmail.com>
2020-06-05 11:45:05 +0100

    Modifies Grid padding for consistency

--- src/components/Grid/config.js
+++ src/components/Grid/config.js
@@ -1,16 +1,16 @@
 export const gridConfig = {
-  // col gutter
-  gutterWidth: {
-    xs: '0 10px',
-    sm: '0 15px',
-  },
-  // container padding
-  containerPadding: {
-    xs: '0 15px',
-  },
   // container max-width
   containerWidth: {
     xs: '100%',
     lg: '1440px',
+  },
+  rowPadding: {
+    xs: '0 10px',
+    sm: '0 15px',
+  },
+  // col gutter
+  gutterWidth: {
+    xs: '0 10px',
+    sm: '0 15px',
   },
 }
