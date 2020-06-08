2ce2d553086df6cdd038140daf64991d6e37e04d
andreferreiradlw <ferreirandre25@gmail.com>
2020-06-08 10:58:01 +0100

    Fixes padding in Footer Secondary List

--- src/components/Footer/FooterSecondary/StyledFooterSecondary.js
+++ src/components/Footer/FooterSecondary/StyledFooterSecondary.js
@@ -57,20 +57,24 @@ const MapsLink = styled(Col)`
 const ListContainer = styled(Col)`
   flex-grow: 0;
 `
+const FooterSecondaryListItem = styled.li`
+  margin-top: 6px;
+`
 
 const FooterSecondaryList = styled.ul`
   display: flex;
   flex-wrap: inherit;
   margin: 6px 0;
 
-  &:not(:last-child) {
+  ${FooterSecondaryListItem} {
     margin-right: 6px;
     padding-right: 12px;
   }
-`
 
-const FooterSecondaryListItem = styled.li`
-  margin-top: 6px;
+  ${FooterSecondaryListItem}:last-child {
+    margin-right: 0;
+    padding-right: 0;
+  }
 `
 
 const FooterAnchor = styled(Anchor)`
