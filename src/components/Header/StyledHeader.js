30b1bc1da8f45196717bf1a2e58842399901aae3
Andre Ferreira <43468798+andreferreiradlw@users.noreply.github.com>
2020-06-17 09:42:31 +0100

    11212 - MaPS Banner (#38)
    * Welsh MaPS Logo
    
    * Refactors Header to Accommodate Banner
    
    * Maps banner structure
    
    * Translation support for Header and Banner

--- src/components/Header/StyledHeader.js
+++ src/components/Header/StyledHeader.js
@@ -1,11 +1,31 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { resolveMedia } from '../../utils/helpers'
-import Container from '../Grid/Container'
-import { Anchor } from '../Anchor/index'
+import { Container, Row, Col } from '../Grid'
+import { Anchor } from '../Anchor'
 
 const HeaderContainer = styled(Container)`
-  background: ${props =>
-    !props.background && props.theme.colors.header.background};
+  & svg {
+    fill: white;
+  }
+`
+
+const HeaderRow = styled(Row)`
+  ${({ background, theme }) =>
+    !background &&
+    css`
+      background: ${theme.colors.header.background};
+    `}
+
+  ${({ padding }) =>
+    !padding &&
+    css`
+      padding: 5px 10px;
+
+      ${resolveMedia.sm`
+        padding: 10px 15px;
+      `}
+    `}
+    
   min-height: 3.375rem;
 
   ${resolveMedia.md`
@@ -17,13 +37,52 @@ const LocaleContainer = styled(Anchor)``
 
 const LogoContainer = styled(Anchor)`
   overflow: hidden;
-  width: 200px;
-  & svg {
-    fill: white;
-  }
+  width: 180px;
+
+  ${resolveMedia.sm`
+    width: 200px;
+  `}
+
   ${resolveMedia.md`
     width: 285px;
   `}
 `
 
-export { HeaderContainer, LogoContainer, LocaleContainer }
+const MapsBannerRow = styled(Row)`
+  ${({ background, theme }) =>
+    !background &&
+    css`
+      background: ${theme.colors.mapsBlue};
+    `}
+`
+
+const MapsLogo = styled(Col)`
+  color: white;
+  padding-top: 13px;
+  padding-bottom: 13px;
+
+  & svg {
+    width: 90px;
+  }
+`
+
+const MapsLogoText = styled.span`
+  margin-right: 10px;
+
+  & span {
+    display: block;
+    text-align: right;
+    font-size: 0.625rem;
+    line-height: 1;
+  }
+`
+
+export {
+  HeaderContainer,
+  HeaderRow,
+  LogoContainer,
+  LocaleContainer,
+  MapsBannerRow,
+  MapsLogo,
+  MapsLogoText,
+}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,56 +1,86 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { genericPropTypes, genericPropsDefaults } from '../../utils/prop-types'
-import { HeaderContainer, LogoContainer, LocaleContainer } from './StyledHeader'
-import { Row, Col } from '../Grid'
+import {
+  HeaderContainer,
+  HeaderRow,
+  LogoContainer,
+  LocaleContainer,
+  MapsBannerRow,
+  MapsLogo,
+  MapsLogoText,
+} from './StyledHeader'
+import { Col } from '../Grid'
+// logos
 import MasLogoEng from '../../assets/Images/mas_logo_en.svg'
 import MasLogoCy from '../../assets/Images/mas_logo_cy.svg'
+import MapsLogoEng from '../../assets/Images/maps_logo_en.svg'
+import MapsLogoCy from '../../assets/Images/maps_logo_cy.svg'
+// translations
+import LocaleEn from './locale_en.json'
+import LocaleCy from './locale_cy.json'
 
 const Header = ({
   a11yTitle,
   as,
   children,
-  currentLgn,
+  currentLng,
   lngUrl,
-  localeText,
+  i18nLng,
   setLng,
   ...rest
 }) => {
+  // locale
+  const lng = i18nLng || (currentLng === 'cy' ? LocaleCy : LocaleEn)
+
   return (
     <>
       <HeaderContainer fluid forwardedAs={as} {...rest}>
-        <Row
-          justify="space-between"
-          align="center"
-          padding="12px 15px"
-          margin="auto"
-          constrained
-        >
-          <Col grow={false}>
-            <LogoContainer
-              href="https://www.moneyadviceservice.org.uk/en"
-              margin="0"
+        <MapsBannerRow justify="center">
+          <Col direction="row" justify="flex-end" align="center" constrained>
+            <MapsLogo
+              grow={false}
+              direction="row"
+              justify="flex-end"
+              align="center"
             >
-              {currentLgn === 'en' ? <MasLogoEng /> : <MasLogoCy />}
-            </LogoContainer>
+              <MapsLogoText>
+                <span>{lng.mapsLogo.key1}</span>
+                <span>{lng.mapsLogo.key2}</span>
+              </MapsLogoText>
+              {currentLng === 'en' ? <MapsLogoEng /> : <MapsLogoCy />}
+            </MapsLogo>
           </Col>
-          <Col grow={false}>
-            <LocaleContainer
-              color="white"
-              textSize="0.875rem"
-              weight="500"
-              margin="0"
-              onClick={setLng}
-              href={lngUrl}
-            >
-              {localeText
-                ? localeText
-                : currentLgn === 'en'
-                ? 'Cymraeg'
-                : 'English'}
-            </LocaleContainer>
+        </MapsBannerRow>
+        <HeaderRow justify="center" align="center">
+          <Col
+            direction="row"
+            justify="space-between"
+            align="center"
+            constrained
+          >
+            <Col grow={false}>
+              <LogoContainer
+                href="https://www.moneyadviceservice.org.uk/en"
+                margin="0"
+              >
+                {currentLng === 'cy' ? <MasLogoCy /> : <MasLogoEng />}
+              </LogoContainer>
+            </Col>
+            <Col grow={false}>
+              <LocaleContainer
+                color="white"
+                textSize="0.875rem"
+                weight="500"
+                margin="0"
+                onClick={setLng}
+                href={lngUrl}
+              >
+                {lng.localeButton}
+              </LocaleContainer>
+            </Col>
           </Col>
-        </Row>
+        </HeaderRow>
       </HeaderContainer>
       {children}
     </>
@@ -60,11 +90,11 @@ const Header = ({
 // Documentation
 Header.propTypes = {
   /** Current Language Value */
-  currentLgn: PropTypes.oneOf(['en', 'cy']),
+  currentLng: PropTypes.oneOf(['en', 'cy']),
   /** Server-side fallback url to change language */
   lngUrl: PropTypes.string,
-  /** Text to be displayed in Locale Button */
-  localeText: PropTypes.string,
+  /** Alternate translations for header. */
+  i18nLng: PropTypes.object,
   /** Function to trigger by Locale Button */
   setLng: PropTypes.func,
   ...genericPropTypes,
@@ -72,7 +102,7 @@ Header.propTypes = {
 
 Header.defaultProps = {
   as: 'header',
-  currentLgn: 'en',
+  currentLng: 'en',
   ...genericPropsDefaults(),
 }
 
