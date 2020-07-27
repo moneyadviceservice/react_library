0541bd6423cd82754b51ad63a46b243291bd2cf3
andreferreiradlw <ferreirandre25@gmail.com>
2020-07-27 15:36:52 +0100

    CompanyCard structure and contact section

--- src/components/CompanyCard/StyledCompanyCard.js
+++ src/components/CompanyCard/StyledCompanyCard.js
@@ -1,6 +1,60 @@
 import styled, { css } from 'styled-components'
-import { Col } from '../Grid'
+// import components
+import { Row, Col } from '../Grid'
+import { Heading } from '../Heading'
+import Button from '../Button'
+import { Paragraph } from '../Paragraph'
 
-const CardContainer = styled(Col)``
+const CardContainer = styled(Col)`
+  ${({ margin }) =>
+    !margin &&
+    css`
+      margin-bottom: 20px;
+    `}
+  ${({ padding }) =>
+    !padding &&
+    css`
+      padding: 15px 0;
+    `}
+  ${({ border }) =>
+    !border &&
+    css`
+      border: 1px solid #edf0f2;
+      border-radius: 5px;
+    `}
+  
+  box-shadow: 5px 5px 5px rgba(215, 215, 215, 1);
+`
 
-export { CardContainer }
+const CardRow = styled(Row)`
+  padding: 0;
+`
+
+const CardCol = styled(Col)``
+
+const CompanyTitle = styled(Heading)`
+  color: #003d8e;
+  border-bottom: 1px solid #edf0f0;
+`
+
+const SubHeading = styled(Heading)``
+
+const CardButton = styled(Button)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  background-color: transparent;
+  color: #003d8e;
+  border: 1px solid #003d8e;
+  border-radius: 5px;
+  margin-bottom: 10px;
+
+  & svg {
+    fill: #003d8e;
+    width: 20px;
+    margin-right: 10px;
+  }
+`
+
+export { CardContainer, CardRow, CardCol, CompanyTitle, SubHeading, CardButton }
--- src/components/CompanyCard/index.js
+++ src/components/CompanyCard/index.js
@@ -1,18 +1,69 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { genericPropTypes, genericPropsDefaults } from '../../utils/prop-types'
-import { CardContainer } from './StyledCompanyCard'
+import {
+  CardContainer,
+  CardRow,
+  CardCol,
+  CompanyTitle,
+  SubHeading,
+  CardButton,
+} from './StyledCompanyCard'
+// svg icons
+import PhoneIcon from '../../assets/Images/phone_volume.svg'
+import ExternalLinkIcon from '../../assets/Images/external_link.svg'
+import EnvelopeIcon from '../../assets/Images/envelope.svg'
 
-const CompanyCard = ({ a11yTitle, children, ...rest }) => {
+const CompanyCard = ({ a11yTitle, children, data, ...rest }) => {
+  const { firmName, getInTouch, moreInfo } = data
+
+  const { phone, website, email } = getInTouch
+
+  const {
+    medicalCondiditon,
+    ageLimit,
+    destination,
+    durationLimit,
+    treatmentStage,
+  } = moreInfo
+
   return (
-    <CardContainer aria-label={a11yTitle} {...rest}>
-      {children}
+    <CardContainer aria-label={a11yTitle || firmName} {...rest}>
+      <CardRow>
+        <CardCol>
+          <CompanyTitle level={2} margin="0">
+            {firmName}
+          </CompanyTitle>
+        </CardCol>
+      </CardRow>
+      <CardRow>
+        <CardCol sizes={{ xs: 12, sm: 4 }}>
+          <SubHeading level={4}>Get in touch</SubHeading>
+          <CardButton href={`tel:${phone}`}>
+            <PhoneIcon />
+            {phone}
+          </CardButton>
+          <CardButton href={website} target="_blank">
+            <ExternalLinkIcon />
+            Website
+          </CardButton>
+          <CardButton href={`mailto:${email}`}>
+            <EnvelopeIcon />
+            Email
+          </CardButton>
+        </CardCol>
+        <CardCol sizes={{ xs: 12, sm: 8 }}>
+          <SubHeading level={4}>More info</SubHeading>
+        </CardCol>
+      </CardRow>
     </CardContainer>
   )
 }
 
 // Documentation
 CompanyCard.propTypes = {
+  /** Company data to be displayed inside the card. */
+  data: PropTypes.object,
   ...genericPropTypes,
 }
 
