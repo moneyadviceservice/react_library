18f28ca8904c3d4121c95f399fcfa033ba753ae5
andreferreiradlw <ferreirandre25@gmail.com>
2020-10-20 15:42:27 +0100

    Header component tests

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { render, fireEvent } from '../../utils/testing'
+import { Header } from '../Header'
+
+describe('<Header />', () => {
+  describe('Snapshots', () => {
+    it('should match snapshot', () => {
+      const { container } = render(<Header />)
+      expect(container).toMatchSnapshot()
+    })
+    it('should match snapshot in Welsh', () => {
+      const { container } = render(<Header currentLng="cy" />)
+      expect(container).toMatchSnapshot()
+    })
+  })
+
+  describe('lngUrl prop', () => {
+    it('should change the anchor url', () => {
+      const { container, getByTestId } = render(
+        <Header lngUrl="https://www.test.com/" />
+      )
+      // snapshot
+      expect(container).toMatchSnapshot()
+      // href
+      expect(getByTestId('localeAnchor').href).toBe('https://www.test.com/')
+    })
+  })
+
+  describe('setLng prop', () => {
+    it('should call function on anchor click', () => {
+      const onClickMock = jest.fn()
+      const { container, getByTestId } = render(<Header setLng={onClickMock} />)
+      // snapshot
+      expect(container).toMatchSnapshot()
+
+      fireEvent.click(getByTestId('localeAnchor'))
+
+      expect(onClickMock).toHaveBeenCalled()
+    })
+  })
+})
